c8b1d273304831b8b61b9e82ba529d314195cc7e
i-Blaise <menniablaise@yahoo.com>
2024-06-10 14:43:41 +0000

    middleware for staging

---
+++ app/Http/Middleware/RedirectToComingSoon.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectToComingSoon
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $stagingURL = 'staging.interactivedigital.com.gh';
+
+        if ($request->getHost() !== $stagingURL)
+        {
+            $newUrl = $request->getScheme() . '://' . $request->getHost().'/coming-soon';
+            return redirect($newUrl);
+        }
+
+        return $next($request);
+    }
+}
