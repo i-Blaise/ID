2a8e0fd0b35e7ec2d869e476becda66152cc7cbd
i-Blaise <menniablaise@yahoo.com>
2024-09-08 20:28:15 +0000

    navbar. blogs

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,7 +10,7 @@
             <li class="menu-link"><a class="{{ Route::currentRouteNamed('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a></li>
             <li class="menu-link"><a class="{{ Route::currentRouteNamed('aboutus') ? 'active' : '' }}" href="{{ route('aboutus') }}">About Us</a></li>
             <li class="menu-link"><a class="{{ Route::currentRouteNamed('services') || Route::currentRouteNamed('sm-marketing') || Route::currentRouteNamed('online-advertising')|| Route::currentRouteNamed('videography') || Route::currentRouteNamed('strategy') ? 'active' : '' }}" href="{{ route('services') }}">Services</a></li>
-            <li class="menu-link"><a class="{{ Route::currentRouteNamed('blogs') ? 'active' : '' }}" href="{{ route('blogs') }}">Blog</a></li>
+            <li class="menu-link"><a class="{{ Route::currentRouteNamed('blogs') ? 'active' : '' }}" href="{{ route('blogs') }}">Blogs</a></li>
             <li class="menu-link"><a class="{{ Route::currentRouteNamed('case-study') ||
             Route::currentRouteNamed('case-open')? 'active' : '' }}" href="{{ route('case-study') }}">Case Studies</a></li>
             <li class="menu-link call-us"><a href="{{ route('contactus') }}">
