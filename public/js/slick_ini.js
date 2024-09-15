b22ce18f98aa9683bc2d7b86081f003a5c620501
i-Blaise <menniablaise@yahoo.com>
2024-09-15 17:13:50 +0000

    slick responsiveness 1

--- public/js/slick_ini.js
+++ public/js/slick_ini.js
@@ -6,9 +6,27 @@ $(".regular").slick({
   slidesToScroll: 1,
   autoplay: true,
   arrows: false,
-  mobileFirst: true,
   swipe: true,
-  pauseOnFocus: false
+  pauseOnFocus: false,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
@@ -20,5 +38,7 @@ $(".lazy").slick({
     nextArrow: '<button type="button" class="next-btn"><img src="../assets/homepage/right.svg" alt=""></button>',
     dots: true,
     // autoplay: true,
-    dotsClass: 'testimonial-dots'
+    dotsClass: 'testimonial-dots',
+    mobileFirst: true
+
   });
